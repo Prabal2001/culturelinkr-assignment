0aa3c3cdfafaeebe3c4f038498962f821ce52d6a
Prabal Arora <99201484+Prabal2001@users.noreply.github.com>
2024-10-25 13:22:46 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,4 @@ app.get("/",(req,res) => {
  res.send("hello backend todo");
 })
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port:${PORT}`);
-});
+module.exports = app;
