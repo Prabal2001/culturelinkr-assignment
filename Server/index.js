0899de68f90865d39f041b20b680fecdeeffd07b
Prabal Arora <99201484+Prabal2001@users.noreply.github.com>
2024-10-25 12:54:18 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const todoroute = require("./routes/todoroute");
 const app = express();
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/todos", todoroute);
@@ -11,9 +12,6 @@ app.use("/api/todos", todoroute);
 app.get("/",(req,res)=>{
     res.send("hello,this is backend");
 }
-
-connectDB();
-
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
