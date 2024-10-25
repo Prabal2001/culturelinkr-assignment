4ea85a92801c553f0926d1caf65e77e9ee27e45c
Prabal Arora <99201484+Prabal2001@users.noreply.github.com>
2024-10-25 12:18:32 +0530

    Update todoroute.js

--- Server/routes/todoroute.js
+++ Server/routes/todoroute.js
@@ -3,6 +3,7 @@ const todoController = require("../controllers/todocontroller");
 
 const router = express.Router();
 
+router.get("/",todoController.homeTodo);
 router.get("/get-todo", todoController.getTodo);
 router.post("/add-todo", todoController.addToDo);
 
